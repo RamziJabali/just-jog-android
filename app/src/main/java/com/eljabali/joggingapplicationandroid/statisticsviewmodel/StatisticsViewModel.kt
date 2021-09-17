432e637d0c1b3a1a8305bbf35563db672a00a970
RamziJabali <ramzieljabali@gmail.com>
2021-09-17 16:18:35 -0700

    add clock timer

--- app/src/main/java/com/eljabali/joggingapplicationandroid/statisticsviewmodel/StatisticsViewModel.kt
+++ app/src/main/java/com/eljabali/joggingapplicationandroid/statisticsviewmodel/StatisticsViewModel.kt
@@ -2,23 +2,39 @@ package com.eljabali.joggingapplicationandroid.statisticsviewmodel
 
 import androidx.lifecycle.ViewModel
 import com.eljabali.joggingapplicationandroid.statisticsview.StatisticsViewState
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.subjects.BehaviorSubject
 import zoneddatetime.ZonedDateTimes
 import zoneddatetime.extensions.print
+import java.util.concurrent.TimeUnit
 
 class StatisticsViewModel : ViewModel() {
 
-    private var statisticsViewState = StatisticsViewState()
-
     val observableStatisticsViewState = BehaviorSubject.create<StatisticsViewState>()
 
+    private var statisticsViewState = StatisticsViewState()
+    private val compositeDisposable = CompositeDisposable()
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+    }
+
     fun onFragmentLaunch() {
-        val now = ZonedDateTimes.now
-        statisticsViewState = statisticsViewState.copy(
-            time = now.print("HH:mm"),
-            date = now.print("EEE, MMM d yyyy")
-        )
-        invalidateView()
+        compositeDisposable.add(Observable.interval(1, TimeUnit.SECONDS)
+            .timeInterval()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                val now = ZonedDateTimes.now
+                statisticsViewState = statisticsViewState.copy(
+                    time = now.print("HH:mm:ss"),
+                    date = now.print("EEE, MMM d yyyy")
+                )
+                invalidateView()
+            })
+
     }
 
     private fun invalidateView() {
