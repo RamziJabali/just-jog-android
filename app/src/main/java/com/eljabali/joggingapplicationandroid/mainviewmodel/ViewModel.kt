c19bf22a9a8073a1daa0cd890a586ebb9bc8553c
RamziJabali <ramzieljabali@gmail.com>
2021-09-25 13:35:36 -0700

    Updated getDatesFromModifiedJogInformation() to now show missed dates by showing a red tile instead of white or greenn

--- app/src/main/java/com/eljabali/joggingapplicationandroid/mainviewmodel/ViewModel.kt
+++ app/src/main/java/com/eljabali/joggingapplicationandroid/mainviewmodel/ViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.util.Log
+import androidx.core.content.contentValuesOf
 import androidx.lifecycle.AndroidViewModel
 import com.eljabali.joggingapplicationandroid.mainview.ColoredDates
 import com.eljabali.joggingapplicationandroid.mainview.ViewState
@@ -57,9 +58,11 @@ class ViewModel(application: Application, private val useCase: UseCase) :
 
     private fun getDatesFromModifiedJogInformation(listOfModifiedJogDateInformation: List<ModifiedJogDateInformation>): List<ColoredDates> {
         val listOfDates: MutableList<ColoredDates> = mutableListOf()
+        val listOfZonedDateTime: MutableList<ZonedDateTime> = mutableListOf()
         if (listOfModifiedJogDateInformation.isNotEmpty()) {
             var tempDate = listOfModifiedJogDateInformation[0].dateTime
             listOfDates.add(ColoredDates(getDateFromZonedDateTime(tempDate), hasWorkedOutColor))
+            listOfZonedDateTime.add(tempDate)
             for (element in listOfModifiedJogDateInformation) {
                 if (tempDate.year != element.dateTime.year || tempDate.dayOfYear != element.dateTime.dayOfYear) {
                     tempDate = element.dateTime
@@ -69,35 +72,30 @@ class ViewModel(application: Application, private val useCase: UseCase) :
                             hasWorkedOutColor
                         )
                     )
+                    listOfZonedDateTime.add(tempDate)
                 }
+            }
+            tempDate = listOfModifiedJogDateInformation[0].dateTime
+            var index = 0
+            while (tempDate.year != ZonedDateTime.now().year || tempDate.dayOfYear != ZonedDateTime.now().dayOfYear) {
+                if (tempDate.year != listOfZonedDateTime[index].year || tempDate.dayOfYear != listOfZonedDateTime[index].dayOfYear) {
+                    listOfDates.add(
+                        ColoredDates(
+                            getDateFromZonedDateTime(tempDate),
+                            hasNotWorkedOutColor
+                        )
+                    )
+                    listOfZonedDateTime.add(tempDate)
+                } else {
+                    index++
+                }
+                tempDate = tempDate.plusDays(1)
             }
             return listOfDates
         }
         return listOfDates
     }
-
-
-//    private fun getDatesFromModifiedJogInformation(listOfModifiedJogDateInformation: List<ModifiedJogDateInformation>): List<ColoredDates> {
-//        val listOfDates: MutableList<ColoredDates> = mutableListOf()
-//        if (listOfModifiedJogDateInformation.isNotEmpty()) {
-//            var tempDate = listOfModifiedJogDateInformation[0].dateTime
-//            listOfDates.add(ColoredDates(getDateFromZonedDateTime(tempDate), hasWorkedOutColor))
-//            for (element in listOfModifiedJogDateInformation) {
-//                if (tempDate.year != element.dateTime.year || tempDate.dayOfYear != element.dateTime.dayOfYear) {
-//                    tempDate = element.dateTime
-//                    listOfDates.add(
-//                        ColoredDates(
-//                            getDateFromZonedDateTime(tempDate),
-//                            hasWorkedOutColor
-//                        )
-//                    )
-//                }
-//            }
-//            return listOfDates
-//        }
-//        return listOfDates
-//    }
-
+    
     private fun getDateFromZonedDateTime(dateTime: ZonedDateTime): Date =
         Date.from(dateTime.toInstant())
 
