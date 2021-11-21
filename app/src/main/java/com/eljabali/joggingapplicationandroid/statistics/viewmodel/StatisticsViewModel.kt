f4f45cbefd7617cf840e2a31f200610319c2a127
Ramzi Jabali <ramzieljabali@gmail.com>
2021-11-21 18:34:48 +0300

    Added chart dependencies, fixed daily tab

--- app/src/main/java/com/eljabali/joggingapplicationandroid/statistics/viewmodel/StatisticsViewModel.kt
+++ app/src/main/java/com/eljabali/joggingapplicationandroid/statistics/viewmodel/StatisticsViewModel.kt
@@ -11,10 +11,10 @@ import com.eljabali.joggingapplicationandroid.data.usecase.ModifiedJogSummary
 import com.eljabali.joggingapplicationandroid.statistics.view.StatisticsViewState
 import com.eljabali.joggingapplicationandroid.statistics.view.WeeklyStats
 import com.eljabali.joggingapplicationandroid.util.*
+import io.reactivex.android.schedulers.AndroidSchedulers
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
 import io.reactivex.schedulers.Schedulers
