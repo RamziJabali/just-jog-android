715f273208994a09301c78f4e3300794f3c02e81
Ramzi Jabali <ramzi@eljabali.org>
2024-04-29 08:02:07 -0700

    Fixed theming

--- app/src/main/java/ramzi/eljabali/justjog/MainActivity.kt
+++ app/src/main/java/ramzi/eljabali/justjog/MainActivity.kt
@@ -21,7 +21,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JustJogTheme(true) {
-                val data = listOf(LineData(x = "Sun", y = 200), LineData(x = "Mon", y = 40))
+                val data = listOf(
+                    LineData(x = "Mon", y = 40),
+                    LineData(x = "Tues", y = 60),
+                    LineData(x = "Wed", y = 70),
+                    LineData(x = "Thurs", y = 120),
+                    LineData(x = "Fri", y = 80),
+                    LineData(x = "Sat", y = 60),
+                    LineData(x = "Sun", y = 150),
+                )
                 StatisticsPage("Try your best until you succeed - RJ!", data)
             }
         }
