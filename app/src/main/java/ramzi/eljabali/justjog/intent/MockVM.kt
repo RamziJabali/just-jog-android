a82e9ea5863e15894023d0347b9f5ae940fac398
Ramzi Eljabali <ramzi@eljabali.org>
2024-05-14 13:01:14 -0700

    [JJ-11] Choose Architecture for application (#43)
    Chose to use MVI, reference article to understand why [MVVM vs MVI](https://github.com/RamziJabali/Core-Android-Features/blob/main/MVVM-vs-MVI.md)

--- app/src/main/java/ramzi/eljabali/justjog/intent/MockVM.kt
+++ app/src/main/java/ramzi/eljabali/justjog/intent/MockVM.kt
@@ -1,4 +1,4 @@
-package ramzi.eljabali.justjog.viewmodels
+package ramzi.eljabali.justjog.intent
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
@@ -8,8 +8,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import ramzi.eljabali.justjog.model.state.UserState
 import ramzi.eljabali.justjog.repository.room.jogentries.JogEntry
 import ramzi.eljabali.justjog.repository.room.jogentries.JogEntryDAO
 import kotlin.coroutines.cancellation.CancellationException
@@ -19,13 +19,6 @@ class MockVM(private val dao: JogEntryDAO) : ViewModel() {
     private val _userStateFlow = MutableStateFlow(UserState())
     val userStateFlow: StateFlow<UserState> = _userStateFlow
 
-    fun userClickJogButton() {
-        _userStateFlow.update {
-            it.copy(
-                didUserPressJogButton = true
-            )
-        }
-    }
 
     fun addJog(jogEntry: JogEntry) {
         val deferred = CompletableDeferred<Unit>()
@@ -82,27 +75,4 @@ class MockVM(private val dao: JogEntryDAO) : ViewModel() {
             }
         }
     }
-
-    fun needUserPermissions() {
-        _userStateFlow.update {
-            it.copy(
-                didUserPressJogButton = false,
-                doesNeedToCheckPermission = true
-            )
-        }
-    }
-
-    fun userClickedGetAllJogButton() {
-        _userStateFlow.update {
-            it.copy(
-                didUserClickGetAllJogsButton = true
-            )
-        }
-    }
-
-    data class UserState(
-        val didUserPressJogButton: Boolean = false,
-        val doesNeedToCheckPermission: Boolean = false,
-        val didUserClickGetAllJogsButton: Boolean = false
-    )
 }
