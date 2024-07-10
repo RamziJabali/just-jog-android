72e28207f948b97d6100c282654abf457daa9778
Ramzi Jabali <ramzi@eljabali.org>
2024-07-09 21:15:48 -0700

    Removed commented code from setting.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,24 +12,24 @@ pluginManagement {
     }
 }
 
-//dependencyResolutionManagement {
-//    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-//    repositories {
-//        google()
-//        mavenCentral()
-//        maven(url = "https://jitpack.io")
-//
-//        // Mapbox Maven repository
-//        maven {
-//            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
-//            // Do not change the username below. It should always be "mapbox" (not your username).
-//            credentials.username = "mapbox"
-//            // Use the secret token stored in gradle.properties as the password
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+
+        // Mapbox Maven repository
+        maven {
+            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
+            // Do not change the username below. It should always be "mapbox" (not your username).
+            credentials.username = "mapbox"
+            // Use the secret token stored in gradle.properties as the password
 //            credentials.password = providers.gradleProperty("MAPBOX_DOWNLOADS_TOKEN").get()
-//            authentication.create<BasicAuthentication>("basic")
-//        }
-//    }
-//}
+            authentication.create<BasicAuthentication>("basic")
+        }
+    }
+}
 
 rootProject.name = "JustJog"
 include(":app")
