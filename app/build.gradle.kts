c4eecca4546809ce8fb276c729d4b93dcbcea1dc
Ramzi Jabali <ramzi@eljabali.org>
2024-07-09 21:01:04 -0700

    Removed unused librarier

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.play.services.maps)
     implementation(libs.androidx.foundation.layout.android)
     implementation(libs.androidx.foundation.layout.android)
     implementation(libs.androidx.foundation.layout.android)
